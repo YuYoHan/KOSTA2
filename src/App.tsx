619d42b760d34b1092b2075fb9115a2d5239bbda
YuYoHan <zxzz8014@naver.com>
2024-07-31 20:09:47 +0900

    [Study] ts 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import './App.css';
-import UseState from "./components/hooks/useState";
-import Time from "./components/hooks/useState2";
-import UseState3 from "./components/hooks/useState3";
+import React from "react";
+import "./App.css";
 
 function App() {
     return (
         <>
-            <UseState/>
-            <Time/>
-            <UseState3/>
+            <header className="App-header"></header>
         </>
     );
 }
