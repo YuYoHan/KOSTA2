6f4bdc79caa0a04304601d039ac9b255f7ef7406
YuYoHan <zxzz8014@naver.com>
2024-08-03 21:28:59 +0900

    [Study] ESLint & Prettier 사용

---
+++ src/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    // 사용자 정의 규칙 추가 가능
+    "react/react-in-jsx-scope": "off",
+  },
+  plugins: ["react", "@typescript-eslint"],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import React from "react";
-import "./App.css";
+import styled from "@emotion/styled";
+import { keyframes, Keyframes } from "@emotion/react";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+
+  @ketframes App-logo-spin {
+    from {
+      tranform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const spinAnimation = keyframes`
+    from {
+        transform: rotate(0deg);
+    } to {
+        transform: rotate(360deg);
+    }
+`;
 
 function App() {
-    return (
-        <>
-            <header className="App-header"></header>
-        </>
-    );
+  return (
+    <>
+      <AppLogo src="{logo}" alt="logo" />
+      <Container>
+        <Header>
+          <h1>React App with Emotion</h1>
+        </Header>
+      </Container>
+    </>
+  );
 }
 
 export default App;
